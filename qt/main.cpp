95bb61b5de206de5206d3bb3af3db68c40aa4e85
Eshan Roy (Eshanized) <m.eshanized@gmail.com>
2024-04-30 23:28:15 +0530

    fix: typo

--- qt/main.cpp
+++ qt/main.cpp
@@ -5,7 +5,7 @@
 int main(int argc, char* argv[])
 {
     QApplication a(argc, argv);
-    SetupAssistant w(nullptr, a.arguments().length() > 1 ? a.arguments()[1] : "");
+    SnigdhaOSAssistant w(nullptr, a.arguments().length() > 1 ? a.arguments()[1] : "");
     w.show();
     return a.exec();
 }
