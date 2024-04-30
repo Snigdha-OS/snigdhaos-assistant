0bb15c2631ee9f0d8043b74d9e633f9a754c2727
Eshan Roy (Eshanized) <m.eshanized@gmail.com>
2024-04-30 22:16:25 +0530

    chore: update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,10 +2,10 @@
 
 #include <QApplication>
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
     QApplication a(argc, argv);
-    SnigdhaOSAssistant w;
+    SetupAssistant w(nullptr, a.arguments().length() > 1 ? a.arguments()[1] : "");
     w.show();
     return a.exec();
 }
