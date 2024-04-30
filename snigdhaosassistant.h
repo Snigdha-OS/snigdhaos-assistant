477a4f97c5c8910ce13ee326287577d5a197c311
Eshan Roy (Eshanized) <m.eshanized@gmail.com>
2024-04-30 22:24:52 +0530

    chore: update header

--- snigdhaosassistant.h
+++ snigdhaosassistant.h
@@ -8,6 +8,8 @@
 #define SNIGDHAOSASSISTANT_H
 
 #include <QMainWindow>
+#include <QAbstractButton>
+#include <QtNetwork/QNetworkAccessManager>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -20,10 +22,42 @@ class SnigdhaOSAssistant : public QMainWindow
     Q_OBJECT
 
 public:
-    SnigdhaOSAssistant(QWidget *parent = nullptr);
+    enum class State {
+        QUIT,
+        WELCOME,
+        INTERNET,
+        UPDATE,
+        UPDATE_RETRY,
+        NVIDIA_CHECK,
+        NVIDIA,
+        NVIDIA_APPLY,
+        SELECT,
+        APPLY,
+        APPLY_RETRY,
+        SUCCESS
+    };
+
+    SnigdhaOSAssistant(QWidget* parent = nullptr, QString state = "WELCOME");
     ~SnigdhaOSAssistant();
+
+private slots:
+    void on_textWidget_buttonBox_clicked(QAbstractButton* button);
+    void on_selectWidget_buttonBox_clicked(QAbstractButton* button);
+
 
 private:
     Ui::SnigdhaOSAssistant *ui;
+    QDateTime executable_modify_date;
+    State currentState;
+    void doInternetUpRequest();
+    void doUpdate();
+    void doApply();
+    void doNvidiaCheck();
+    void doNvidiaApply();
+    void populateSelectWidget();
+    void populateSelectWidget(QString filename, QString label);
+    void updateState(State state);
+    void updateState(QString state);
+    void relaunchSelf(QString param);
 };
 #endif // SNIGDHAOSASSISTANT_H
