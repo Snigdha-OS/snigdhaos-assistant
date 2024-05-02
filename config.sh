daf53aed620046a738343ae9a6b1fc13dd7ca91a
Eshan Roy (Eshanized) <m.eshanized@gmail.com>
2024-05-02 22:21:36 +0530

    refactor(config): create my own git credentials

---
+++ config.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Author : Eshan Roy <eshan@snigdhaos.org>
+# Author URL: https://eshanized.github.io/
+
+# Setting up Author's Git Credentials
+echo
+echo "################################################################################"
+echo -e "If you have write Access, create another file excluding the name 'config.sh'"
+echo "################################################################################"
+echo
+
+# Git Credentials:
+git config --global pull.rebase false # Merge
+git config --global user.name "Eshan Roy (Eshanized)"
+git config --global user.email "m.eshanized@gmail.com"
+
+echo
+echo "################################################################################"
+echo -e "If you have write Access, create another file excluding the name 'config.sh'"
+echo "################################################################################"
+echo
