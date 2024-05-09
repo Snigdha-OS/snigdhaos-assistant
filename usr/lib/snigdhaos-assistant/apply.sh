786b5bf6795ed3cdfb5e97cd11b59f521fe6f37b
Eshan Roy (Eshanized) <m.eshanized@gmail.com>
2024-05-10 03:08:12 +0530

    style(emojis): add emojis

--- usr/lib/snigdhaos-assistant/apply.sh
+++ usr/lib/snigdhaos-assistant/apply.sh
@@ -1,22 +1,22 @@
 if [ -e "$1" ]; then
 	echo ""
-	echo "Preparing setup.."
+	echo "Preparing setup ⏳"
 	echo ""
 	sudo bash - <$1
 fi
 
 echo ""
-echo "Installing packages.."
+echo "Installing packages ⏳"
 echo ""
 installable_packages=$(comm -12 <(pacman -Slq | sort) <(sed s/\\s/\\n/g - <$2 | sort))
 sudo pacman -S --needed $installable_packages && rm $2 || { read -p "Error! Press enter to return to Snigdha OS Assistant."; exit; }
 
 if [ -e "$3" ]; then
 	echo ""
-	echo "Enabling services.."
+	echo "Enabling services ⏳"
 	echo ""
 	sudo bash - <$3
 fi
 
 echo ""
-read -p "Press enter to return to Snigdha OS Assistant."
+read -p "Press enter to return to Snigdha OS Assistant ⤶"
